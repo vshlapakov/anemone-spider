90ea81a498a99ce9f9a8d4802caf5bffdde2fcc8
Daniel Graña <dangra@gmail.com>
2014-03-11 13:40:29 -0300

    wip

--- spider.rb
+++ spider.rb
@@ -13,6 +13,7 @@ HUBSTORAGE = ENV.fetch('SHUB_STORAGE', 'https://storage.scrapinghub.com')
 
 class Uploader
 
+  attr_accessor :jobkey
   attr_accessor :prefix
   attr_accessor :endpoint
   attr_accessor :offset
@@ -20,7 +21,9 @@ class Uploader
   attr_accessor :batchsize
   attr_accessor :count
 
-  def initialize(prefix='items', endpoint=HUBSTORAGE)
+  def initialize(key, auth, prefix='items', endpoint=HUBSTORAGE)
+    @key = key
+    @auth = auth
     @prefix = prefix
     @endpoint = endpoint
     @offset = 0
@@ -46,25 +49,30 @@ class Uploader
 
   private
   def upload(prefix, items, params)
-    uri = URI("#{HUBSTORAGE}/#{prefix}/#{JOBDATA['key']}")
+    uri = URI("#{@endpoint}/#{@prefix}/#{@key}")
     uri.query = URI.encode_www_form(params)
     http = Net::HTTP.new(uri.host, uri.port)
     req = Net::HTTP::Post.new(uri.request_uri)
-    req.basic_auth(JOBDATA['key'], JOBDATA['auth'])
+    req.basic_auth(@auth[0], @auth[1])
     req.body = items.map(&:to_json).join('\n')
     req.content_type = 'application/x-jsonlines'
-    #puts req.body
+    puts "#{req.method} #{req.path} #{items.length} #{req.body.length}"
     res = http.request(req)
+    puts "#{res.code} #{res.body()}"
   end
 end
 
 
-itemup = Uploader.new('items')
-logup = Uploader.new('logs')
+jobkey = JOBDATA['key']
+auth = [JOBDATA['key'], JOBDATA['auth']]
+auth = ['vagrant', ENV['HSAPIKEY']]
+puts auth
+itemup = Uploader.new(jobkey, auth, 'items')
+logup = Uploader.new(jobkey, auth, 'logs')
+
 TITLE = "//div[@class='pmovie']/h1"
 DESC = "//div[@class='pmovie']/div[@class='details']"
 IMAGE = "//div[@class='pmovie']/img[@class='poster']"
-
 Anemone.crawl("http://www.argenteam.net/") do |anemone|
   anemone.on_pages_like(/movie\/\d+\/.*/) do |page|
     title = page.doc.at_xpath(TITLE).text rescue nil
@@ -77,8 +85,8 @@ Anemone.crawl("http://www.argenteam.net/") do |anemone|
     itemup.add(item)
     logup.add({:message => "Crawled #{page.url}"})
 
-    # Give up after 15 items
-    if itemup.count > 15
+    # Give up after 7 items
+    if itemup.count > 7
       itemup.commit()
       exit
     end
